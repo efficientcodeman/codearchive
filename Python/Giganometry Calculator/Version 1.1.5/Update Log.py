f1d4b1da323601fe94d8a2cf45ad0c9ae2310c73
Filip <filipila2012@gmail.com>
2025-02-08 18:13:22 +0000

    Create Update Log.py

---
+++ Python/Giganometry Calculator/Version 1.1.5/Update Log.py
@@ -0,0 +1,3 @@
+#v1.0          -New Calculator
+#v1.1.3.1.4.1  -Pi Update :D
+#v1.5          -Fixed csc, sec and cot being sin, cos and tan (1.5.6 Will have a reset (clear) button and a random button (first number is minimum range second max range))
